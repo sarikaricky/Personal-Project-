553a5acd2f0426e1c175e614602f32f66fbc9b17
sarika <sarikaresume@gmail.com>
2017-12-28 14:09:28 -0800

    button adding

--- src/signin.js
+++ src/signin.js
@@ -3,6 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super(this); 
+    this.state = {
+      text: "Initial text"
+    };
+  }
+
+  clicked(text) {
+    this.setState({ text: this.refs.textBox.val() });
+
   render() {
     return (
       <div className="App">
@@ -15,7 +26,18 @@ class App extends Component {
         </p>
       </div>
     );
+  render () 
+    console.log('rendering')
+    const img = assets("./images/sign-i.njpg")
+
+    return<div>
+      { this.state.text }
+      <img src={img} />
+      <input ref="textBox" type="text" />
+      <button onClick={ (e) => {this.clicked("hello"); } }>TheButton</button>
+      </div>;
   }
 }
+    
 
 export default App;
